f2c2c4a094059f44ea2e6fbd93de01d82337a97c
saeedmgo <saeedmoghanllo@gmail.com>
2021-10-28 13:28:05 +0330

    replas-container

--- src/component/Container/Container.js
+++ src/component/Container/Container.js
@@ -2,9 +2,5 @@ import { Col, Row } from "antd";
 import React from "react";
 
 export default function Container() {
-  return (
-    <Row>
-      <Col xs={12}></Col>
-    </Row>
-  );
+  return <div>Container</div>;
 }
